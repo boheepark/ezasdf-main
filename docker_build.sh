082817100674eb0109cc627fceaedff417c23de9
Bohee Park <boheepark91@gmail.com>
2017-12-06 08:21:00 -0500

    docker build

---
+++ docker_build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+docker login -u $DOCKER_ID -p $DOCKER_PASSWORD
+docker pull $DOCKER_ID/$USERS
+docker pull $DOCKER_ID/$USERS_DB
+docker pull $DOCKER_ID/$CLIENT
+docker pull $DOCKER_ID/$SWAGGER
+docker pull $DOCKER_ID/$NGINX
+
+docker-compose -f docker-compose-ci.yml up -d --build
