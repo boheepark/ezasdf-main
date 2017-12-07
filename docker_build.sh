2f439af819acca6fc444489d46e9992ffbb91e5c
Bohee Park <boheepark91@gmail.com>
2017-12-07 00:21:21 -0500

    development -> $TAG

--- docker_build.sh
+++ docker_build.sh
@@ -3,12 +3,13 @@
 
 if [ "$TRAVIS_BRANCH" == "development" ];
 then
+    export TAG=$TRAVIS_BRANCH
     docker login -u $DOCKER_ID -p $DOCKER_PASSWORD
-    docker pull $DOCKER_ID/$USERS:development
-    docker pull $DOCKER_ID/$USERS_DB:development
-    docker pull $DOCKER_ID/$CLIENT:development
-    docker pull $DOCKER_ID/$SWAGGER:development
-    docker pull $DOCKER_ID/$NGINX:development
+    docker pull $DOCKER_ID/$USERS:$TAG
+    docker pull $DOCKER_ID/$USERS_DB:$TAG
+    docker pull $DOCKER_ID/$CLIENT:$TAG
+    docker pull $DOCKER_ID/$SWAGGER:$TAG
+    docker pull $DOCKER_ID/$NGINX:$TAG
 fi
 
 docker-compose -f docker-compose-ci.yml up -d --build
