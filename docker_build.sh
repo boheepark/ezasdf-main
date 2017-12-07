4a590c308606fc95652af1b2ce8ef4d79a120fef
Bohee Park <boheepark91@gmail.com>
2017-12-06 23:50:44 -0500

    development?

--- docker_build.sh
+++ docker_build.sh
@@ -4,11 +4,11 @@
 if [ "$TRAVIS_BRANCH" == "development" ];
 then
     docker login -u $DOCKER_ID -p $DOCKER_PASSWORD
-    docker pull $DOCKER_ID/$USERS
-    docker pull $DOCKER_ID/$USERS_DB
-    docker pull $DOCKER_ID/$CLIENT
-    docker pull $DOCKER_ID/$SWAGGER
-    docker pull $DOCKER_ID/$NGINX
+    docker pull $DOCKER_ID/$USERS:development
+    docker pull $DOCKER_ID/$USERS_DB:development
+    docker pull $DOCKER_ID/$CLIENT:development
+    docker pull $DOCKER_ID/$SWAGGER:development
+    docker pull $DOCKER_ID/$NGINX:development
 fi
 
 docker-compose -f docker-compose-ci.yml up -d --build
