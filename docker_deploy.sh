0de140f5fb5248f0117668d38e0393ed9b7f3569
Bohee Park <boheepark91@gmail.com>
2017-12-08 05:55:03 -0500

    Now, the service will automatically be updated with the new Task Definition revision

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -32,9 +32,20 @@ then
         }
 
         deploy_cluster() {
+
             family="testdriven-staging"
+            cluster="ezasdf-staging"
+            service="ezasdf-staging"
+
             make_task_def
             register_definition
+
+            if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]];
+            then
+                echo "Error updating service."
+                return 1
+            fi
+
         }
 
         configure_aws_cli
